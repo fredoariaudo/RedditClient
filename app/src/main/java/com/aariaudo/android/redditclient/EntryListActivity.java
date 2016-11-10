46bce928fd784c9b7f5fc92647a91d2b6f10de23
Alfredo Ariaudo <fredoariaudo@msn.com>
2016-11-10 00:16:23 -0300

    Add RecyclerView to EntryListActivity

--- app/src/main/java/com/aariaudo/android/redditclient/EntryListActivity.java
+++ app/src/main/java/com/aariaudo/android/redditclient/EntryListActivity.java
@@ -2,6 +2,8 @@ package com.aariaudo.android.redditclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class EntryListActivity extends AppCompatActivity
 {
@@ -10,5 +12,8 @@ public class EntryListActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_list);
+
+        RecyclerView rvEntryList = (RecyclerView) findViewById(R.id.rv_entry_list);
+        rvEntryList.setLayoutManager(new LinearLayoutManager(this));
     }
 }

f6e4489660fcd02620e38d3cc3b5e4261a495835
Alfredo Ariaudo <fredoariaudo@msn.com>
2016-11-14 15:48:41 -0300

    Fix rounded corners on image load

--- app/src/main/java/com/aariaudo/android/redditclient/adapters/EntryRvAdapter.java
+++ app/src/main/java/com/aariaudo/android/redditclient/adapters/EntryRvAdapter.java
@@ -113,7 +113,7 @@ public class EntryRvAdapter extends ArrayRvAdapter<RedditEntry>
             entryViewHolder.tvEliTitle.setText(entry.getTitle());
             entryViewHolder.tvEliAuthorDate.setText(String.format(entryViewHolder.tvEliAuthorDate.getContext().getResources().getString(R.string.eli_author_date), entry.getAuthor(), dateString));
             entryViewHolder.tvEliComments.setText(String.valueOf(entry.getComments()));
-            Glide.with(entryViewHolder.ivEliThumbnail.getContext()).load(entry.getThumbnail()).placeholder(R.drawable.entry_default).centerCrop().into(entryViewHolder.ivEliThumbnail);
+            Glide.with(entryViewHolder.ivEliThumbnail.getContext()).load(entry.getThumbnail()).centerCrop().error(R.drawable.entry_default).into(entryViewHolder.ivEliThumbnail);
         }
     }
 }
